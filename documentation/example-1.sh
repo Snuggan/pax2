7d279a89eea87f563faff5a35a5882f58330cf78
Peder Axensten <peder@axensten.se>
2024-02-18 11:51:02 +0100

    Edited docs.

--- documentation/example-1.sh
+++ documentation/example-1.sh
@@ -5,43 +5,46 @@
 # well as creating individual point cloud files for all points in each plot. 
 # 
 # Say we have the following files:
-# - plots.csv is the input plots file.
-# - points.laz is the input point cloud file.
-# - raster-metrics.tif is the output raster metrics file.
-# - plot-metrics.csv is the output plot metrics file.
-# - plot-points-files is the output plot points *directory*.
 
-PDAL_METRICS_PIPELINE := {	"pipeline":[ \
-	{ \
-		"type":"filters.raster_metrics", \
-		"resolution":"12.5", \
-		"metrics":"extra-allt", \
-		"nilsson_level":"1.5", \
-		"gdalopts":"BIGTIFF=IF_SAFER,COMPRESS=DEFLATE", \
-		"data_type":"float" \
-	},{ \
-		"type":"filters.plot_metrics", \
-		"plot_file":"plots.csv", \
-		"metrics":"extra-allt", \
-		"nilsson_level":"1.5" \
-	},{ \
-		"type":"filters.plot_points", \
-		"plot_file":"plots.csv", \
-		"dest_plot_points":"plot-points-files", \
-		"max_distance":"12", \
-		"id_column":"Descriptiontxt", \
-		"dest_format":".laz" \
-	} ] }
+INPUT_PLOTS_FILE="the-input-plots-file.csv"
+INPUT_POINTS_FILE="the-input-point-cloud-file.laz"
+OUTPUT_RASTER_METRICS="the-output-raster-metrics/template.tif"
+OUTPUT_PLOTS_METRICS="the-output-plot-metrics-file.csv"
+OUTPUT_PLOTS_POINTS_DIR="the-output-plot-points-directory/"
 
-# Then we can calculate raster metrics for points.laz and for all 
-# plots in plots.csv we calculate metrics and save individual point 
+PDAL_METRICS_PIPELINE='{ "pipeline":[
+	{
+		"type":"filters.raster_metrics",
+		"resolution":"12.5",
+		"metrics":"extra-allt",
+		"nilsson_level":"1.5",
+		"gdalopts":"BIGTIFF=IF_SAFER,COMPRESS=DEFLATE",
+		"data_type":"float"
+	},{
+		"type":"filters.plot_metrics",
+		"plot_file":"${INPUT_PLOTS_FILE}",
+		"metrics":"extra-allt",
+		"nilsson_level":"1.5"
+	},{
+		"type":"filters.plot_points",
+		"plot_file":"${INPUT_PLOTS_FILE}",
+		"dest_plot_points":"${OUTPUT_PLOTS_POINTS_DIR}",
+		"max_distance":"12",
+		"id_column":"Descriptiontxt",
+		"dest_format":".laz"
+	} ] }'
+
+# Then we can calculate raster metrics for ${INPUT_POINTS_FILE} and for all 
+# plots in ${INPUT_PLOTS_FILE} we calculate metrics and save individual point 
 # cloud files using the command:
 
+echo "${PDAL_METRICS_PIPELINE}"
+
 pdal translate \
-	--json=$PDAL_METRICS_PIPELINE \
-	--input=points.laz \
+	--json="${PDAL_METRICS_PIPELINE}" \
+	--input="${INPUT_POINTS_FILE}" \
 	--output=null.bull \
 	--writer=writers.null \
-	--filters.raster_metrics.dest=metrics.tif \
-	--filters.plot_metrics.dest_plot_metrics=plots-metrics.csv \
+	--filters.raster_metrics.dest="${OUTPUT_RASTER_METRICS}" \
+	--filters.plot_metrics.dest_plot_metrics="${OUTPUT_PLOTS_METRICS}" \
 	--metadata=.json
