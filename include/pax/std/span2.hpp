4ce21ebe905fba9aa52e60734aeec7446ea790ae
Peder Axensten <peder@axensten.se>
2024-05-17 16:05:31 +0200

    Optimized a few details.

--- include/pax/std/span2.hpp
+++ include/pax/std/span2.hpp
@@ -19,19 +19,23 @@ namespace pax {
 	template< typename T, std::size_t N = std::dynamic_extent >
 	class span2 : public std::span< T, N > {
 		using     base = std::span< T, N >;
-
+		
 #if( __cpp_lib_span >= 202311L )
 		using base::at;		// Came in C++26, might throw an exception.
 #endif
 		
-		
+		template< std::integral Int >
 		[[nodiscard]] static constexpr std::size_t subspan_offset( 
-			const std::ptrdiff_t 	offset_, 
+			const Int			 	offset_, 
 			const std::size_t 		size_ 
 		) noexcept {
-			return	(  offset_ >= 0 )					  ? std::min( std::size_t( offset_ ), size_ ) 
-				:	( std::size_t( -offset_ ) < size_ )	  ? size_ - std::size_t( -offset_ )
-				:											std::size_t{};
+			if constexpr( std::is_unsigned_v< Int > ) {
+				return	std::min( offset_, size_ );
+			} else {
+				return	( offset_ >= 0 )					  ? std::min( std::size_t( offset_ ), size_ ) 
+					:	( std::size_t( -offset_ ) < size_ )	  ? size_ - std::size_t( -offset_ )
+					:											std::size_t{};
+			}
 		}
 
 		[[nodiscard]] static constexpr std::size_t charlen( T * c_ ) noexcept {
@@ -40,9 +44,9 @@ namespace pax {
 			return itr - c_;
 		}
 
-		template< std::size_t I >
+		template< std::size_t I = base::extent >
 		static constexpr bool	is_dynamic = ( I == std::dynamic_extent );
-		template< std::size_t I >
+		template< std::size_t I = base::extent >
 		static constexpr bool	is_static  = !is_dynamic< I >;
 
 	public:
@@ -62,15 +66,15 @@ namespace pax {
 
 		/// Turns an array into a static `span2`.
 		template< std::size_t N0 >
-		constexpr span2( T( & c_ )[ N0 ] )	noexcept requires ( is_static < N > && ( N == N0 ) )
+		constexpr span2( T( & c_ )[ N0 ] )	noexcept requires ( is_static <> && ( N == N0 ) )
 			: base( c_, N ) {}
 
 		/// Turns an array of characters into a dynamic `span2`. Does not include a final `'\0'`.
-		constexpr span2( T * & c_ )			noexcept requires ( is_dynamic< N > && Character< T > )
+		constexpr span2( T * & c_ )			noexcept requires ( is_dynamic<> && Character< T > )
 			: base( c_, charlen( c_ ) ) {}
 
 		/// Turns an array of const characters into a dynamic `span2`. Does not include a final `'\0'`.
-		constexpr span2( T * const & c_ )	noexcept requires ( is_dynamic< N > && Character< T > )
+		constexpr span2( T * const & c_ )	noexcept requires ( is_dynamic<> && Character< T > )
 			: base( c_, charlen( c_ ) ) {}
 
 
@@ -87,7 +91,7 @@ namespace pax {
 
 		/// Returns a statically sized `span2` of the first `I` elements.
 		///	- If `I > extent`, `*this` is returned.
-		template< std::size_t I >		requires( is_static< I > && is_static< base::extent > )
+		template< std::size_t I >		requires( is_static< I > && is_static<> )
 		[[nodiscard]] constexpr auto first()									const noexcept {
 			static constexpr std::size_t 	sz = std::min( I, base::extent );
 			return span2< T, sz >( base::data(), sz );
@@ -95,7 +99,7 @@ namespace pax {
 
 		/// Returns a statically sized `span2` of the first `I` elements.
 		///	- There is an `assert( I <= size() )`.
-		template< std::size_t I >		requires( is_static< I > && is_dynamic< base::extent > )
+		template< std::size_t I >		requires( is_static< I > && is_dynamic<> )
 		[[nodiscard]] constexpr auto first()									const noexcept {
 			assert( I <= base::size() && "requires I <= size()." );
 			return span2< T, I >( base::data(), I );
@@ -109,7 +113,7 @@ namespace pax {
 
 		/// Returns a statically sized `span2` of the last `I` elements.
 		///	- If `I > extent`, `*this` is returned.
-		template< std::size_t I >		requires( is_static< I > && is_static< base::extent > )
+		template< std::size_t I >		requires( is_static< I > && is_static<> )
 		[[nodiscard]] constexpr auto last()										const noexcept {
 			static constexpr std::size_t	offset = ( I < base::extent ) ? base::extent - I : 0u;
 			return span2< T, base::extent - offset >( base::data() + offset, base::extent - offset );
@@ -117,7 +121,7 @@ namespace pax {
 
 		/// Returns a statically sized `span2` of the last `I` elements.
 		///	- There is an `assert( I <= size() )`.
-		template< std::size_t I >		requires( is_static< I > && is_dynamic< base::extent > )
+		template< std::size_t I >		requires( is_static< I > && is_dynamic<> )
 		[[nodiscard]] constexpr auto last()										const noexcept {
 			assert( I <= base::size() && "requires I <= size()." );
 			return span2< T, I >( base::end() - I, I );
@@ -132,7 +136,7 @@ namespace pax {
 
 		/// Returns a statically sized `span2` of all elements except the first `I`.
 		///	- If `I > extent`, `span2< T, 0 >( data() + size(), 0 )` is returned.
-		template< std::size_t I >			requires( is_static< I > && is_static< base::extent > )
+		template< std::size_t I >			requires( is_static< I > && is_static<> )
 		[[nodiscard]] constexpr auto not_first()								const noexcept {
 			static constexpr std::size_t	offset = ( I < base::extent ) ? I : base::extent;
 			static constexpr std::size_t	sz = base::extent - offset;
@@ -147,7 +151,7 @@ namespace pax {
 
 		/// Returns a statically sized `span2` of all elements except the last `I`.
 		///	- If `I > extent`, `span< T, 0 >( data(), 0 )` is returned.
-		template< std::size_t I >			requires( is_static< I > && is_static< base::extent > )
+		template< std::size_t I >			requires( is_static< I > && is_static<> )
 		[[nodiscard]] constexpr auto not_last()									const noexcept {
 			static constexpr std::size_t 	sz = ( I < base::extent ) ? base::extent - I : 0u;
 			return span2< T, sz >( base::data(), sz );
@@ -156,8 +160,9 @@ namespace pax {
 		/// Returns a dynamically sized `span2` of `size_` elements starting at `offset_`.
 		///	- If `offset_ < 0`, `offset_ += size()` is used (the offset is seen from the back), 
 		///	- If `offset_ + size_ >= size()`: returns `not_first( offset_ )`.
+		template< std::integral Int >
 		[[nodiscard]] constexpr auto subspan( 
-			const std::ptrdiff_t 			offset_, 
+			const Int			 			offset_, 
 			const std::size_t 				size_ 
 		) const noexcept {
 			const auto 						offset = subspan_offset( offset_, base::size() );
@@ -167,8 +172,8 @@ namespace pax {
 		/// Returns a statically sized `span2` of `Len` elements starting at `offset_`.
 		///	- If `offset_ < 0`, an `offset_ += size()` is used (the offset is seen from the back), 
 		///	- There is an `assert( offset_ + Len <= size() )`.
-		template< std::size_t Len >			requires is_static< Len >
-		[[nodiscard]] constexpr auto subspan( const std::ptrdiff_t offset_ )	const noexcept	{
+		template< std::size_t Len, std::integral Int >		requires is_static< Len >
+		[[nodiscard]] constexpr auto subspan( const Int offset_ )				const noexcept	{
 			const auto 						offset = subspan_offset( offset_, base::size() );
 			assert( offset + Len <= base::size()  && "requires offset_ + Len <= size()." );
 			return span2< T, Len >( base::data() + offset, Len );
@@ -178,7 +183,7 @@ namespace pax {
 		///	- If `Offset < 0`, an offset = `Offset + size()` is used (the offset is seen from the back), 
 		///	- If `Offset + Len > size()`, returns `not_first< offset >()`.
 		template< std::ptrdiff_t Offset, std::size_t Len >	
-			requires( is_static< Len > && is_static< base::extent > )
+			requires( is_static< Len > && is_static<> )
 		[[nodiscard]] constexpr auto subspan()									const noexcept {
 			static constexpr auto			offset = subspan_offset( Offset, base::extent );
 			static constexpr auto 			len    = std::min( base::extent - offset, Len );
@@ -189,7 +194,7 @@ namespace pax {
 		///	- If `Offset < 0`, an offset = `Offset + size()` is used (the offset is seen from the back), 
 		///	- There is an `assert( offset + Len <= size() )`.
 		template< std::ptrdiff_t Offset, std::size_t Len >
-			requires( is_static< Len > && is_dynamic< base::extent > )
+			requires( is_static< Len > && is_dynamic<> )
 		[[nodiscard]] constexpr auto subspan()									const noexcept {
 			const auto 						offset = subspan_offset( Offset, base::size() );
 			assert( offset + Len <= base::size() && "requires Offset + Len <= size()." );
