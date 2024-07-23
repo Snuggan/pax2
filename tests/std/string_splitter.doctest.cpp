c72b97248f2d22585c123e2f0c2c9ee790910d3f
Peder Axensten <peder@axensten.se>
2024-07-23 11:33:53 +0200

    The contents of these files was moved into other.

---
+++ tests/std/string_splitter.doctest.cpp
@@ -0,0 +1,13 @@
+//	Copyright (c) 2014-2016, Peder Axensten, all rights reserved.
+//	Contact: peder ( at ) axensten.se
+
+
+#include <pax/std/algorithm.hpp>
+#include <pax/external/doctest/doctest-pax.hpp>
+// #include <pax/reporting/debug.hpp>
+
+
+namespace pax {
+	
+
+}	// namespace pax
---
+++ tests/tables/table2html.doctest.cpp
@@ -0,0 +1,16 @@
+//	Copyright (c) 2014-2016, Peder Axensten, all rights reserved.
+//	Contact: peder ( at ) axensten.se
+
+
+#include <pax/tables/table2html.hpp>
+
+#include <pax/external/doctest/doctest-pax.hpp>
+
+
+namespace pax {
+	
+	DOCTEST_TEST_CASE( "" ) { 
+		DOCTEST_FAST_CHECK_EQ( 42, 42 );
+	}
+
+}	// namespace pax
