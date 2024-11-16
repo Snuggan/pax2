5d88fb2ef6578388cd5a2e1b387d8e18c5a71141
Peder Axensten <peder@axensten.se>
2024-11-16 15:21:56 +0100

    Lifted out of string_view2 as it concerns std::string_view only.

---
+++ include/pax/std/string_view.hpp
@@ -0,0 +1,30 @@
+//	Copyright (c) 2014-2016, Peder Axensten, all rights reserved.
+//	Contact: peder ( at ) axensten.se
+
+
+#pragma once
+
+#include <string_view>
+
+
+namespace std {
+
+	/// Concatenate a string and a string view.
+	template< typename Ch, typename Traits, typename Allocator >
+	[[nodiscard]] constexpr auto operator+(
+		const basic_string< remove_cv_t< Ch >, Traits, Allocator >	  & str_,
+		const basic_string_view< Ch, Traits >							view_
+	) {
+		return basic_string< remove_cv_t< Ch >, Traits, Allocator >( str_ )+= view_;
+	}
+
+	/// Concatenate a string view and a string.
+	template< typename Ch, typename Traits, typename Allocator >
+	[[nodiscard]] constexpr auto operator+(
+		const basic_string_view< Ch, Traits >							view_, 
+		const basic_string< remove_cv_t< Ch >, Traits, Allocator >	  & str_
+	) {
+		return basic_string< remove_cv_t< Ch >, Traits, Allocator >( view_ )+= str_ ;
+	}
+
+}	// namespace std
