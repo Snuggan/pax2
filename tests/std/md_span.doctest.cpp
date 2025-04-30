ea9399d29cad13d2791d039ac66776d7cb9e737e
Peder Axensten <peder@axensten.se>
2025-04-30 18:39:27 +0200

    Added some test for md_span resize.

--- tests/std/md_span.doctest.cpp
+++ tests/std/md_span.doctest.cpp
@@ -13,12 +13,10 @@
 
 namespace pax {
 
-	constexpr std::array		extents{ 4u, 8u };
+	constexpr std::array		extents{ 8u, 4u };
 	constexpr auto		 		cols = extents[ 0 ];
 	constexpr auto		 		rows = extents[ 1 ];
-	constexpr std::size_t 		sz   = cols*rows;
-	constexpr std::size_t 		maxsz= 64;
-	static  int arr[ maxsz ] = {
+	constexpr std::array		arr{
 		 0,  1,  2,  3,  4,  5,  6,  7,		// 0
 		 8,  9, 10, 11, 12, 13, 14, 15,		// 1
 		16, 17, 18, 19, 20, 21, 22, 23,		// 2
@@ -26,41 +24,41 @@ namespace pax {
 		32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 
 		48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63
 	};
-	constexpr std::span			sp0{ std::span{ arr } };
-	constexpr std::span			sp { std::span{ arr }.first( sz ) };
-	
-	template< std::size_t Dim, typename Layout >
-	using The_span = std::mdspan< int, std::dextents< std::size_t, Dim >, Layout >;
+	constexpr std::span			sp0{ arr };
+	constexpr std::span			sp { sp0.first( cols*rows ) };
 
 
 	DOCTEST_TEST_CASE( "layout" ) { 
 		{	// layout_right
-			using Right		  = std::mdspan< int, std::dextents< std::size_t, 2 >, std::layout_right >;
-			const Right			md( sp.data(), std::array{ cols, rows } );
+			using Right		  = std::mdspan< const int, std::dextents< std::size_t, 2 >, std::layout_right >;
+			const Right			md( sp.data(), extents );
 			DOCTEST_FAST_CHECK_EQ( md.extent( 0 ),	cols );
 			DOCTEST_FAST_CHECK_EQ( md.extent( 1 ),	rows );
 			DOCTEST_FAST_CHECK_EQ( md[ 0, 0 ],		 0 );
 			DOCTEST_FAST_CHECK_EQ( md[ 0, 1 ],		 1 );
 			DOCTEST_FAST_CHECK_EQ( md[ 1, 0 ],		rows );
-			DOCTEST_FAST_CHECK_EQ( md[ 3, 7 ],		rows*cols-1 );
+			DOCTEST_FAST_CHECK_EQ( md[ 7, 3 ],		3*cols + 7 );
 		}
 		{	// layout_left
-			using Right		  = std::mdspan< int, std::dextents< std::size_t, 2 >, std::layout_left >;
-			const Right			md( sp.data(), std::array{ cols, rows } );
+			using Right		  = std::mdspan< const int, std::dextents< std::size_t, 2 >, std::layout_left >;
+			const Right			md( sp.data(), extents );
 			DOCTEST_FAST_CHECK_EQ( md.extent( 0 ),	cols );
 			DOCTEST_FAST_CHECK_EQ( md.extent( 1 ),	rows );
 			DOCTEST_FAST_CHECK_EQ( md[ 0, 0 ],		 0 );
 			DOCTEST_FAST_CHECK_EQ( md[ 0, 1 ],		cols );
 			DOCTEST_FAST_CHECK_EQ( md[ 1, 0 ],		 1 );
-			DOCTEST_FAST_CHECK_EQ( md[ 3, 7 ],		rows*cols-1 );
+			DOCTEST_FAST_CHECK_EQ( md[ 7, 3 ],		3 + rows*7 );
 		}
 	}
 	DOCTEST_TEST_CASE( "resize" ) { 
-		constexpr std::array	downsize{ 3u, 6u }, upsize_r{ 6u, rows }, upsize_c{ cols, 8u }, upsize_rc{ 6u, 8u };
+		constexpr std::array	downsize { cols - 2, rows - 1 };
+		constexpr std::array	upsize_r { cols,     rows + 2 };
+		constexpr std::array	upsize_c { cols + 2, rows };
+		constexpr std::array	upsize_rc{ cols + 2, rows + 2 };
 
 		{	// layout_right
 			using MDspan	  = std::mdspan< int, std::dextents< std::size_t, 2 >, std::layout_right >;
-			constexpr int		in01{ 1 }, in10{ 8 }, in25{ 21 };
+			constexpr int		in01{ arr[ 1 ] }, in10{ arr[ rows ] }, in25{ 2 + rows*5 };
 
 			{	// Downsize
 				constexpr auto						new_extent = downsize;
@@ -71,7 +69,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -79,9 +77,9 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 0 ),	new_extent[ 0 ] );
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 1 ),	new_extent[ 1 ] );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		 1 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		 8 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		21 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
 			}
 			{	// Upsize rows
 				constexpr auto						new_extent = upsize_r;
@@ -92,7 +90,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -100,9 +98,10 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 0 ),	new_extent[ 0 ] );
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 1 ),	new_extent[ 1 ] );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		 1 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		 8 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		21 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, rows ],		 0 );
 			}
 			{	// Upsize cols
 				constexpr auto						new_extent = upsize_c;
@@ -113,7 +112,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -121,9 +120,10 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 0 ),	new_extent[ 0 ] );
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 1 ),	new_extent[ 1 ] );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		 1 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		 8 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		21 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ cols, 0 ],		 0 );
 			}
 			{	// Upsize rows and cols
 				constexpr auto						new_extent = upsize_rc;
@@ -134,7 +134,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -142,14 +142,16 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 0 ),	new_extent[ 0 ] );
 				DOCTEST_FAST_CHECK_EQ( dest.extent( 1 ),	new_extent[ 1 ] );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		 1 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		 8 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		21 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, rows ],		 0 );
+				DOCTEST_FAST_CHECK_EQ( dest[ cols, 0 ],		 0 );
 			}
 		}
 		{	// layout_left
 			using MDspan	  = std::mdspan< int, std::dextents< std::size_t, 2 >, std::layout_left >;
-			constexpr int		in01{ 4 }, in10{ 1 }, in25{ 22 };
+			constexpr int		in01{ arr[ cols ] }, in10{ arr[ 1 ] }, in25{ arr[ cols*2 + 5 ] };
 
 			{	// Downsize
 				constexpr auto						new_extent = downsize;
@@ -160,7 +162,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -170,7 +172,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
 			}
 			{	// Upsize rows
 				constexpr auto						new_extent = upsize_r;
@@ -181,7 +183,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -191,7 +193,8 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, rows ],		 0 );
 			}
 			{	// Upsize cols
 				constexpr auto						new_extent = upsize_c;
@@ -202,7 +205,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -212,7 +215,8 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ cols, 0 ],		 0 );
 			}
 			{	// Upsize rows and cols
 				constexpr auto						new_extent = upsize_rc;
@@ -223,7 +227,7 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( srce[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( srce[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( srce[ 5, 2 ],		in25 );
 
 				MDspan								dest{ data.data(), new_extent };
 				resize( std::span( data ), srce, dest );
@@ -233,7 +237,9 @@ namespace pax {
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 0 ],		 0 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 0, 1 ],		in01 );
 				DOCTEST_FAST_CHECK_EQ( dest[ 1, 0 ],		in10 );
-				DOCTEST_FAST_CHECK_EQ( dest[ 2, 5 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 5, 2 ],		in25 );
+				DOCTEST_FAST_CHECK_EQ( dest[ 0, rows ],		 0 );
+				DOCTEST_FAST_CHECK_EQ( dest[ cols, 0 ],		 0 );
 			}
 		}
 	}
