807ae6cc457de7e74e75fc69ceec563fb5e5f9c3
Peder Axensten <peder@axensten.se>
2025-07-17 15:25:25 +0200

    Estetics.

--- include/pax/meta/tagged_type.hpp
+++ include/pax/meta/tagged_type.hpp
@@ -1,34 +1,38 @@
-//	Copyright (c) 2014-2016, Peder Axensten, all rights reserved.
+//	Copyright (c) Peder Axensten, all rights reserved.
 //	Contact: peder ( at ) axensten.se
 
 
 #pragma once
 
-#include <type_traits>	// std::remove_reference_t, std::size_t, std::int64_t, std::uint64_t etc.
+#include <type_traits>
 
 
 namespace pax {
 
-	/// Use this class to create a type true variable.
-	/// Especially at function calls where you want to be sure not to misstake arguments of the same type.
-	template< typename T, typename Tag, bool Strict = true >
+	/// Class to obfuscate type T. Use at function calls to differentiate arguments of the same type T.
+	/// Tagged< T, ... > is no slower than simply using T. All member functions are constexpr. 
+	/// Usage example: Tagged< bool, struct mybool, false >. 
+	template< 
+		typename T, 		///< Type to obfuscate.
+		typename Tag, 		///< Tag that makes the class implementation unique.
+		bool Strict = true	///< Member function operator T() only activated when Strict is false. 
+	>
 	struct Tagged {
 		using value_type				  = std::remove_reference_t< T >;
 		using tag_type					  = Tag;
 		static constexpr bool is_strict	  = Strict;
 		
-		constexpr Tagged() 						   				noexcept : m_value{} 								{}
+		constexpr Tagged() 						   				noexcept : m_value{} 			{}
 		constexpr Tagged( const Tagged & ) 					  = default;
 		constexpr Tagged & operator=( const Tagged & )		  = default;
-		constexpr Tagged( const value_type value_ )				noexcept : m_value{  value_ } 						{}
+		constexpr Tagged( const value_type value_ )				noexcept : m_value{  value_ } 	{}
 		constexpr Tagged & operator=( const value_type value_ )	noexcept { m_value = value_;		return *this;	 }
-		
-		/// Return m_value when specifically const-accessing a tagged value. 
-		/// Use parameter value Strict to false to use it.
-		constexpr operator value_type()					const   noexcept requires( !is_strict )	{ 	return m_value;	 }
 
-		/// Return m_value when indirectly const-accessing a tagged value. 
+		/// Return m_value when specifically const-accessing a tagged value. 
 		constexpr value_type value()					const   noexcept						{	return m_value;	 }
+		
+		/// Return m_value when indirectly const-accessing a tagged value. Set Strict to false to use it.
+		constexpr operator value_type()					const   noexcept requires( !is_strict )	{ 	return m_value;	 }
 	
 	private:
 		value_type m_value;
@@ -44,8 +48,7 @@ namespace pax {
 	template< typename Tag, bool Strict = true >	using Uint64	= Tagged< std::uint64_t,	Tag, Strict >;
 
 
-	// Some extra floating point types introduced by C++23.
-	// https://en.cppreference.com/w/cpp/types/floating-point.html
+	// Floating point types introduced by C++23: https://en.cppreference.com/w/cpp/types/floating-point.html
 #if __STDCPP_FLOAT16_T__
 	template< typename Tag, bool Strict = true >	using Float16	= Tagged< std::float16_t,	Tag, Strict >;
 #endif
@@ -83,5 +86,4 @@ namespace pax {
 			static_assert( !( Test() = false ) );
 		}
 	}	// namespace only_for_testing_purposes
-
 }	// namespace pax
