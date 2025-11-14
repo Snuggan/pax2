6bfef0bee87d1c3b94682b2d5255b8d14602945e
Peder Axensten <peder@axensten.se>
2025-11-14 18:52:21 +0100

    Estetics.

--- include/pax/std/span.hpp
+++ include/pax/std/span.hpp
@@ -13,8 +13,8 @@ namespace std {
 	/// Check if all elements of `sp_` are equal to the corresponding elements of `v_`.
 	template< typename T, std::size_t N, pax::Contiguous_elements V >
 	[[nodiscard]] constexpr bool operator==(
-		const std::span< T, N >		sp_,
-		V						 && v_
+		const std::span< T, N >	sp_,
+		V					 && v_
 	) noexcept {
 		using std::begin, std::end;
 		return std::equal( begin( sp_ ), end( sp_ ), begin( v_ ), end( v_ ) );
@@ -23,8 +23,8 @@ namespace std {
 	/// Lexicographically compare `sp_` with `v_` using three-way comparison.
 	template< typename T, std::size_t N, pax::Contiguous_elements V >
 	[[nodiscard]] constexpr auto operator<=>(
-		const std::span< T, N >		sp_,
-		V						 && v_
+		const std::span< T, N >	sp_,
+		V					 && v_
 	) noexcept {
 		using std::begin, std::end;
 		return std::lexicographical_compare_three_way( begin( sp_ ), end( sp_ ), begin( v_ ), end( v_ ) );
@@ -34,7 +34,7 @@ namespace std {
 	/// Negative `I` will acces `I + N`.
 	template< std::ptrdiff_t I, typename T, std::size_t N >
 		requires( ( N != std::dynamic_extent ) && ( ( ( I < 0 ) ? -I : I ) < N ) )
-	[[nodiscard]] constexpr T & get( const std::span< T, N > v_ )  					noexcept	{
+	[[nodiscard]] constexpr T & get( const std::span< T, N > v_ )  					noexcept {
 		return *( v_.data() + ( ( I >= 0 ) ? I : I + N ) );
 	}
 }	// namespace std
@@ -43,10 +43,10 @@ namespace std {
 namespace pax {
 
 	template< typename T >
-	concept Not_character_array	  = Contiguous_elements< T > && !Character_array< T >;
+	concept Not_character_array = Contiguous_elements< T > && !Character_array< T >;
 
 	template< typename T >
-	concept Not_string			  = Contiguous_elements< T > && !String< T >;
+	concept Not_string			= Contiguous_elements< T > && !String< T >;
 
 
 	/// Returns a std::span.
@@ -123,8 +123,8 @@ namespace pax {
 	///	- If i_ > size( v_ ), span( v_ ) is returned.
 	template< Not_string V >
 	[[nodiscard]] constexpr auto first( 
-		V								 && v_, 
-		const std::size_t 					i_ = 1 
+		V					 && v_, 
+		const std::size_t 		i_ = 1 
 	) noexcept {
 		using std::data, std::size;
 		return std::span( data( v_ ), std::min( i_, size( v_ ) ) );
@@ -155,8 +155,8 @@ namespace pax {
 	///	- If i_ > size( v_ ), span( v_ ) is returned.
 	template< Not_string V >
 	[[nodiscard]] constexpr auto last( 
-		V								 && v_, 
-		const std::size_t 					i_ = 1 
+		V					 && v_, 
+		const std::size_t 		i_ = 1 
 	) noexcept {
 		using std::data, std::size;
 		return ( i_ < size( v_ ) )	? std::span( data( v_ ) + size( v_ ) - i_, i_ )
@@ -234,8 +234,8 @@ namespace pax {
 	namespace detail {
 		template< std::integral Int >
 		[[nodiscard]] static constexpr std::size_t subview_offset( 
-			const Int			 			offset_, 
-			const std::size_t 				size_ 
+			const Int			offset_, 
+			const std::size_t 	size_ 
 		) noexcept {
 			if constexpr( std::is_unsigned_v< Int > ) {
 				return	std::min( offset_, size_ );
