c587c1be1610b6db4dc4a5f26ff811b4e88da7fb
Peder Axensten <peder@axensten.se>
2025-11-28 16:12:21 +0100

    Added a code hardening flag. It is not yet used by clang, but I guess it will be...

--- build/_debug.sh
+++ build/_debug.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-cmake 											\
-	-DCMAKE_BUILD_TYPE=Debug					\
-	-DCMAKE_INSTALL_RPATH_USE_LINK_PATH="ON"	\
-	-G Ninja									\
+cmake 														\
+	-DCMAKE_BUILD_TYPE=Debug								\
+	-DCMAKE_INSTALL_RPATH_USE_LINK_PATH="ON"				\
+	-D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST	\
+	-G Ninja												\
 	../..
--- build/_release.sh
+++ build/_release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-cmake 									\
-	-DCMAKE_BUILD_TYPE=RelWithDebInfo 	\
-	-G Ninja							\
-	../..
+cmake	-DCMAKE_BUILD_TYPE=RelWithDebInfo						\
+		-D_LIBCPP_HARDENING_MODE=_LIBCPP_HARDENING_MODE_FAST	\
+		../..
