9ba10465a75a3b0c8db996fcc1e3b5756e5d5b9e
Peder Axensten <peder@axensten.se>
2026-01-07 19:19:54 +0100

    shadow is pretty useful now.

--- include/pax/types/shadow.hpp
+++ include/pax/types/shadow.hpp
@@ -4,82 +4,62 @@
 #pragma once
 
 #include <pax/concepts.hpp>		// Character
-#include <pax/std/format.hpp>	// std20::formatter
-#include <algorithm>			// std::equal, std::lexicographical_compare
-#include <string_view>
-#include <span>
+#include <algorithm>			// std::equal, std::lexicographical_compare_three_way
+
+
+// To do:
+// – Implement a struct hard_shadow, where the size is static. 
 
 
 namespace pax {
 	
 	/// Implements the core for span-like utilities.
 	template< typename T >
-	struct core_shadow {
-		using value_type		  = std::remove_cv_t< T >;
+	struct soft_shadow {
+		using element_type		  = T;
+		using value_type		  = std::remove_cv_t< element_type >;
 		using pointer			  = value_type const *;
-		using size_type			  = std::size_t;
-		using difference_type	  = std::ptrdiff_t;
 
-		static constexpr bool		is_string{ Character< value_type > };
+	    constexpr soft_shadow()		noexcept {};
 
-	    constexpr core_shadow()		noexcept		{};
+	    constexpr soft_shadow( const pointer source_, const std::size_t size_ ) noexcept 
+			:	m_source{ source_ }, m_size{ source_ ? size_ : 0u } {}
 
-		template< size_type N >
-	    constexpr core_shadow( value_type const ( & source_ )[ N ] ) noexcept
-			:	m_source{ source_ }, 
-				m_size{ N - ( is_string && ( N > 0 ) && ( source_[ N - 1 ] == 0 ) ) } {}
-
-	    constexpr core_shadow( 
-			const pointer source_, 
-			const size_type size_ 
-		)	noexcept : m_source{ source_ }, m_size{ source_ ? size_ : 0u } {}
+		template< std::size_t N >
+	    constexpr soft_shadow( value_type const ( & source_ )[ N ] ) noexcept
+			:	soft_shadow{ source_, N - ( Character< value_type > && N>0 && !source_[ N - 1 ] ) } {}
 
 		template< Contiguous_elements_object U >
-		constexpr core_shadow( const U & source_ ) noexcept 
-			:	core_shadow{ data( source_ ), size( source_ ) } {}
+		constexpr soft_shadow( const U & source_ ) noexcept 
+			:	soft_shadow{ get_data( source_ ), get_size( source_ ) } {}
 
-		constexpr pointer data()	const noexcept	{	return m_source;				}
-		constexpr size_type size()	const noexcept	{	return m_size;					}
-				
-		template< Contiguous_elements_object U >
-		static constexpr pointer data( const U & source_ ) noexcept {
-			using std::data;
-			return data( source_ );
-		}
-		
-		template< Contiguous_elements_object U >
-		static constexpr size_type size( const U & source_ ) noexcept {
-			using std::data;
-			return size( source_ );
-		}
+		constexpr auto data()		const noexcept	{	return m_source;				}
+		constexpr auto size()		const noexcept	{	return m_size;					}
 
 	private:
 		pointer						m_source{};
-		size_type					m_size{};
+		std::size_t					m_size{};
 	};
 
-	template< typename T, std::size_t N >
-	core_shadow( T ( & )[ N ] ) -> core_shadow< T >;
 	
-
 
 	/// Implements the stuff you want for an interface to contiguous imutable elements.
 	/// The interfaced is similar to that of std::string_view and std::span. 
-	/// Core needs to implement data() and size() and define value_type, size_type, and difference_type.
+	/// Core needs to implement data() and size() and define element_type, value_type, size_type, 
+	/// and difference_type.
 	template< typename Core >
 	struct base_shadow : Core {
-		using value_type		  = typename Core::value_type;
-		using pointer			  = value_type const *;
-		using iterator			  = pointer;
-		using size_type			  = typename Core::size_type;
-		using difference_type	  = typename Core::difference_type;
-		using shadow			  = base_shadow< core_shadow< value_type > >;
+		using element_type			  = typename Core::element_type;
+		using value_type			  = typename Core::value_type;
+		using pointer				  = value_type const *;
+		using iterator				  = pointer;
+		using size_type				  = std::size_t;
+		using difference_type		  = std::ptrdiff_t;
+		using shadow				  = base_shadow< soft_shadow< element_type > >;
 		
-		static constexpr bool		is_string{ Character< value_type > };
+		static constexpr bool			is_string{ Character< value_type > };
 
-	    using Core::Core;
-		using Core::data;
-		using Core::size;
+	    using Core::Core, Core::data, Core::size;
  
 	    constexpr value_type operator[]( const size_type i_ )		const noexcept
 		{	return ( i_ < size() ) ? data()[ i_ ] : value_type{};							}
@@ -107,12 +87,11 @@ namespace pax {
 	    constexpr auto operator<=>( const U & u_ )					const noexcept			{
 	    	using std::begin, std::end;
 			const auto b = begin( u_ );
-	    	return std::lexicographical_compare( this->begin(), this->end(), b, ender( b, end( u_ ) ) );
+	    	return std::lexicographical_compare_three_way( this->begin(), this->end(), b, ender( b, end( u_ ) ) );
 	    }
 		
-		constexpr shadow first( const size_type n_ )				const noexcept			{
-			return { data(), ( size() >= n_ ) ? n_ : size() };
-		}
+		constexpr shadow first( const size_type n_ )				const noexcept
+		{	return { data(), ( size() >= n_ ) ? n_ : size() };								}
  
 		constexpr shadow not_first( size_type n_ )					const noexcept			{
 			n_ = ( size() > n_ ) ? n_ : size();
@@ -124,15 +103,12 @@ namespace pax {
 			return shadow{ data() + size() - n_, n_ };
 		}
  
-		constexpr shadow not_last( const size_type n_ )				const noexcept			{
-			return shadow{ data(), size() - ( ( size() > n_ ) ? n_ : size() ) };
-		}
+		constexpr shadow not_last( const size_type n_ )				const noexcept
+		{	return shadow{ data(), size() - ( ( size() > n_ ) ? n_ : size() ) };			}
  
-		constexpr shadow part(
-			size_type		 	offs_, 
-			const size_type 	n_ = size()
-		) const noexcept {
-			offs_ = ( size() >= offs_ ) ? offs_ : size();
+		constexpr shadow part( difference_type offs_, const size_type n_ ) const noexcept	{
+			offs_ =	( offs_ >= 0 )					  ? std::min( size_type(  offs_ ), size() ) 
+				  :	( size_type( -offs_ ) < size() )  ? size()  - size_type( -offs_ ) : 0u;
 			return { data() + offs_, ( size() >= offs_ + n_ ) ? n_ : size() - offs_ };
 		}
 
@@ -148,9 +124,8 @@ namespace pax {
 			return last( vw.size() ) == vw;
 		}
  
-	    constexpr bool contains( const value_type t_ )				const noexcept			{
-	    	return std::ranges::contains( this->begin(), this->end(), t_ );
-	    }
+	    constexpr bool contains( const value_type t_ )				const noexcept
+		{	return std::ranges::contains( this->begin(), this->end(), t_ );				    }
 
  	    template< typename U >
 	    constexpr bool contains( const U & u_ )						const noexcept			{
@@ -160,36 +135,46 @@ namespace pax {
 	    }
  
 		template< typename Out >
-		friend Out & operator<<( Out & out_, const base_shadow & shadow_ )
-		{	return out_ << std20::format( "{}", shadow_ );									}
+		friend Out & operator<<( Out & out_, const base_shadow & sh_ )						{
+			if constexpr ( is_string )
+				return out_.write( sh_.data(), sh_.size() );
+			else {
+				bool comma = false;
+				out_ << '[';
+				for( const auto & item : sh_ ) {
+					out_ << ( comma ? ", " : "" ) << item;
+					comma = true;
+				}
+				return out_ << ']';
+			}
+		}
 	};
 	
-	
 	template< typename T >
-	using shadow = base_shadow< core_shadow< T > >;
+	using shadow = base_shadow< soft_shadow< T > >;
 
 	template< typename T, std::size_t N >
-	base_shadow( T ( & )[ N ] ) -> base_shadow< core_shadow< T > >;
+	base_shadow( T ( & )[ N ] ) -> base_shadow< soft_shadow< T > >;
 
-	template< Contiguous_elements_object T >
-	base_shadow( const T & ) -> base_shadow< core_shadow< typename T::data_type > >;
+	template< typename T >
+	base_shadow( T const *, std::size_t ) -> base_shadow< soft_shadow< T > >;
+
+	template< Contiguous_elements_object Cont >
+	base_shadow( const Cont & ) -> base_shadow< soft_shadow< typename Cont::data_type > >;
 
 
 
-
-	template< Character Char, size_t N, typename Traits = std::char_traits< Char > >
+	template< Character Char, std::size_t N, typename Traits = std::char_traits< Char > >
 	struct core_litteral {
-		using value_type					  = std::remove_cv_t< Char >;
+		using element_type					  = Char;
+		using value_type					  = std::remove_cv_t< element_type >;
 		using pointer						  = value_type const *;
-		using size_type						  = std::size_t;
-		using difference_type				  = std::ptrdiff_t;
 		using traits_type					  = Traits;
-		static constexpr size_type extent	  = N - 1u;
 
 	    constexpr core_litteral( value_type const ( & str_ )[ N ] )	{	std::copy_n( str_, N, value );	}
 
 		constexpr pointer data()				const noexcept	{	return value;					}
-		static constexpr size_type size()		noexcept		{	return extent;					}
+		static constexpr std::size_t size()		noexcept		{	return N - ( N > 0 );			}
 		
 		value_type								value[ N ];
 	};
@@ -201,30 +186,3 @@ namespace pax {
 	constexpr litteral< Char, N, Traits > litt( Char ( & source_ )[ N ] ) {	return { source_ };		}
 	
 }	// namespace pax
-
-namespace std20 {
-	template< typename T >		requires( pax::base_shadow< T >::is_string )
-	struct formatter< pax::base_shadow< T > > : formatter< basic_string_view< typename T::value_type > > {
-	    constexpr auto format( const pax::base_shadow< T > & shadow_, format_context & ctx_ )	const {
-			// std::format_to_n( ctx_.out(), shadow_.size(), "{}", shadow_.data() );
-			// return ctx_.out();
-			return formatter< basic_string_view< typename T::value_type > >
-				::format( basic_string_view{ shadow_.data(), shadow_.size() }, ctx_ );
-	    }
-	};
-
-	template< typename T >
-	struct formatter< pax::base_shadow< T > > : formatter< span< typename T::value_type > > {
-	    constexpr auto format( const pax::base_shadow< T > & shadow_, format_context & ctx_ )	const {
-			return formatter< span< typename T::value_type > >
-				::format( span{ shadow_.data(), shadow_.size() }, ctx_ );
-	    }
-	};
-
-	// template< typename T >
-	// struct formatter< pax::base_shadow< T > > : formatter< T const * > {
-	//     constexpr auto format( const pax::base_shadow< T > & str_, format_context & ctx_ )	const {
-	// 		return formatter< T const * >::format( str_.value, ctx_ );
-	//     }
-	// };
-}	// namespace std
--- tests/types/shadow.doctest.cpp
+++ tests/types/shadow.doctest.cpp
@@ -4,96 +4,109 @@
 
 #include <pax/types/shadow.hpp>
 #include <pax/doctest.hpp>
+#include <span>
 #include <print>
 
 
 namespace pax {
 
 	template< auto V >	struct litteral_test	{	static constexpr decltype( V ) v = V;	};
+	
+	template< typename Sh >
+	void text_test( const Sh & sh_, const char * const name_ ) {
+		static_assert( std::ranges::contiguous_range< Sh > );
+
+		std::cout << "---- Text:    " << name_ << " -------------------------\n";
+		DOCTEST_FAST_CHECK_EQ ( std::format( "{}", sh_ ), "['t', 'e', 'x', 't']" );
+		DOCTEST_FAST_CHECK_EQ ( std::format( "{:s}", sh_ ), "text" );
+
+		DOCTEST_FAST_CHECK_EQ( sh_					, "text" );
+		DOCTEST_FAST_CHECK_EQ( sh_.size()			, 4u );
+		DOCTEST_FAST_CHECK_GT( sh_					, "test" );
+		DOCTEST_FAST_CHECK_GE( sh_					, "test" );
+		DOCTEST_FAST_CHECK_EQ( sh_[ 3 ]				, 't' );
+		DOCTEST_FAST_CHECK_EQ( *( sh_.begin() + 1 ) , 'e' );
+		DOCTEST_FAST_CHECK_EQ( *( sh_.end()   - 2 ) , 'x' );
+		DOCTEST_FAST_CHECK_EQ( sh_.front() 			, 't' );
+		DOCTEST_FAST_CHECK_EQ( sh_.back() 			, 't' );
+		DOCTEST_FAST_CHECK_EQ( sh_.first( 2 )		, "te" );
+		DOCTEST_FAST_CHECK_EQ( sh_.first( 9 )		, "text" );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_first( 2 )	, "xt" );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_first( 9 )	, "" );
+		DOCTEST_FAST_CHECK_EQ( sh_.last( 2 )		, "xt" );
+		DOCTEST_FAST_CHECK_EQ( sh_.last( 9 )		, "text" );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_last( 2 )	, "te" );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_last( 9 )	, "" );
+		DOCTEST_FAST_CHECK_EQ( sh_.part(  1, 2 )	, "ex" );
+		DOCTEST_FAST_CHECK_EQ( sh_.part(  9, 2 )	, "" );
+		DOCTEST_FAST_CHECK_EQ( sh_.part(  1, 9 )	, "ext" );
+		DOCTEST_FAST_CHECK_EQ( sh_.part( -3, 2 )	, "ex" );
+		DOCTEST_FAST_CHECK_EQ( sh_.part( -9, 2 )	, "te" );
+		DOCTEST_FAST_CHECK_EQ( sh_.part( -3, 9 )	, "ext" );
+		DOCTEST_FAST_CHECK_UNARY( sh_.starts_with( "tex" ) );
+		DOCTEST_FAST_CHECK_UNARY( sh_.ends_with( "ext" ) );
+		DOCTEST_FAST_CHECK_UNARY( sh_.contains( 'x' ) );
+		DOCTEST_FAST_CHECK_UNARY( sh_.contains( "xt" ) );
+	}
+	
+	template< typename Sh >
+	void num_test( const Sh & sh_, const char * const name_ ) {
+		static_assert( std::ranges::contiguous_range< Sh > );
+
+		static constexpr int						  nums[] = { 0, 1, 2, 3, 4, 5, 0 };
+		static constexpr std::span< int >			  empty{};
+
+		std::cout << "---- Numbers: " << name_ << " -------------------------\n";
+		DOCTEST_FAST_CHECK_EQ ( std::format( "{}", sh_ ), "[0, 1, 2, 3, 4, 5, 0]" );
+		DOCTEST_FAST_CHECK_EQ ( std::format( "{::4}", sh_ ), "[   0,    1,    2,    3,    4,    5,    0]" );
+
+		DOCTEST_FAST_CHECK_EQ( sh_					, nums );
+		DOCTEST_FAST_CHECK_EQ( sh_.size()			, 7u );
+		DOCTEST_FAST_CHECK_GT( sh_					, std::array{ 0, 1, 2, 3, 3, 5, 0 } );
+		DOCTEST_FAST_CHECK_GE( sh_					, std::array{ 0, 1, 2, 3, 3, 5, 0 } );
+		DOCTEST_FAST_CHECK_EQ( sh_[ 3 ]				, 3 );
+		DOCTEST_FAST_CHECK_EQ( *( sh_.begin() + 1 ) , 1 );
+		DOCTEST_FAST_CHECK_EQ( *( sh_.end()   - 2 ) , 5 );
+		DOCTEST_FAST_CHECK_EQ( sh_.front() 			, 0 );
+		DOCTEST_FAST_CHECK_EQ( sh_.back() 			, 0 );
+		DOCTEST_FAST_CHECK_EQ( sh_.first( 2 )		, std::array{ 0, 1 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.first( 9 )		, nums );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_first( 2 )	, std::array{ 2, 3, 4, 5, 0 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_first( 9 )	, empty );
+		DOCTEST_FAST_CHECK_EQ( sh_.last( 2 )		, std::array{ 5, 0 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.last( 9 )		, nums );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_last( 2 )	, std::array{ 0, 1, 2, 3, 4 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.not_last( 9 )	, empty );
+		DOCTEST_FAST_CHECK_EQ( sh_.part(  1, 2 )	, std::array{ 1, 2 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.part(  9, 2 )	, empty );
+		DOCTEST_FAST_CHECK_EQ( sh_.part(  1, 9 )	, std::array{ 1, 2, 3, 4, 5, 0 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.part( -3, 2 )	, std::array{ 4, 5 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.part( -9, 2 )	, std::array{ 0, 1 } );
+		DOCTEST_FAST_CHECK_EQ( sh_.part( -3, 9 )	, std::array{ 4, 5, 0 } );
+		DOCTEST_FAST_CHECK_UNARY( sh_.starts_with( std::array{ 0, 1, 2 } ) );
+		DOCTEST_FAST_CHECK_UNARY( sh_.ends_with( std::array{ 4, 5, 0 } ) );
+#if defined( __clang__ )	// Not constexpr in gcc:
+		DOCTEST_FAST_CHECK_UNARY( sh_.contains( 5 ) );
+		DOCTEST_FAST_CHECK_UNARY( sh_.contains( std::array{ 2, 3, 4 } ) );
+#endif
+	}
 
 	DOCTEST_TEST_CASE( "shadow text" ) {
-		DOCTEST_FAST_CHECK_EQ ( std::format( "{}", shadow( "text" ) ), "text" );
-
-		static_assert( shadow( "text" )						== "text" );
-		static_assert( shadow( "text" ).size()				== 4u );
-		// static_assert( shadow( "text" )						>  "test" );
-		static_assert( shadow( "text" )[ 3 ]				== 't' );
-		static_assert( *( shadow( "text" ).begin() + 1 ) 	== 'e' );
-		static_assert( *( shadow( "text" ).end()   - 2 ) 	== 'x' );
-		static_assert( shadow( "text" ).front() 			== 't' );
-		static_assert( shadow( "text" ).back() 				== 't' );
-		static_assert( shadow( "text" ).first( 2 )			== "te" );
-		static_assert( shadow( "text" ).first( 9 )			== "text" );
-		static_assert( shadow( "text" ).not_first( 2 )		== "xt" );
-		static_assert( shadow( "text" ).not_first( 9 )		== "" );
-		static_assert( shadow( "text" ).last( 2 )			== "xt" );
-		static_assert( shadow( "text" ).last( 9 )			== "text" );
-		static_assert( shadow( "text" ).not_last( 2 )		== "te" );
-		static_assert( shadow( "text" ).not_last( 9 )		== "" );
-		static_assert( shadow( "text" ).part( 1, 2 )		== "ex" );
-		static_assert( shadow( "text" ).part( 9, 2 )		== "" );
-		static_assert( shadow( "text" ).part( 1, 9 )		== "ext" );
-		static_assert( shadow( "text" ).starts_with( "tex" ) );
-		static_assert( shadow( "text" ).ends_with( "ext" ) );
-#if defined( __clang__ )	// Not constexpr in gcc:
-		static_assert( shadow( "text" ).contains( 'x' ) );
-		static_assert( shadow( "text" ).contains( "xt" ) );
-#endif
+		static_assert( shadow( "text" ).last( 2 ) == "xt" );
+		DOCTEST_FAST_CHECK_EQ ( std::format( "{:?s}", shadow( "1\t2\n3\"4" ) ), "\"1\\t2\\n3\\\"4\"" );
+		text_test( shadow( "text" ), "shadow( \"text\" )" );
 	}
 	DOCTEST_TEST_CASE( "shadow numbers" ) {
-		// static constexpr std::array		nums{ 0, 1, 2, 3, 4, 5, 0 };
-		// DOCTEST_FAST_CHECK_EQ ( std::format( "{}", shadow( nums ) ), "text" );
-
-// 		static_assert( shadow( nums )					== "text" );
-// 		static_assert( shadow( nums ).size()			== 4u );
-// 		// static_assert( shadow( nums )					>  "test" );
-// 		static_assert( shadow( nums )[ 3 ]				== 't' );
-// 		static_assert( *( shadow( nums ).begin() + 1 ) 	== 'e' );
-// 		static_assert( *( shadow( nums ).end()   - 2 ) 	== 'x' );
-// 		static_assert( shadow( nums ).front() 			== 't' );
-// 		static_assert( shadow( nums ).back() 			== 't' );
-// 		static_assert( shadow( nums ).first( 2 )		== "te" );
-// 		static_assert( shadow( nums ).first( 9 )		== "text" );
-// 		static_assert( shadow( nums ).not_first( 2 )	== "xt" );
-// 		static_assert( shadow( nums ).not_first( 9 )	== "" );
-// 		static_assert( shadow( nums ).last( 2 )			== "xt" );
-// 		static_assert( shadow( nums ).last( 9 )			== "text" );
-// 		static_assert( shadow( nums ).not_last( 2 )		== "te" );
-// 		static_assert( shadow( nums ).not_last( 9 )		== "" );
-// 		static_assert( shadow( nums ).part( 1, 2 )		== "ex" );
-// 		static_assert( shadow( nums ).part( 9, 2 )		== "" );
-// 		static_assert( shadow( nums ).part( 1, 9 )		== "ext" );
-// 		static_assert( shadow( nums ).starts_with( "tex" ) );
-// 		static_assert( shadow( nums ).ends_with( "ext" ) );
-// #if defined( __clang__ )	// Not constexpr in gcc:
-// 		static_assert( shadow( nums ).contains( 5 ) );
-// 		static_assert( shadow( nums ).contains( {{ 3, 4 }} ) );
-// #endif
+		static constexpr int				nums[] = { 0, 1, 2, 3, 4, 5, 0 };
+		// static constexpr std::array			nums{ 0, 1, 2, 3, 4, 5, 0 };
+		static_assert( shadow( nums ).last( 2 ) == std::array{ 5, 0 } );
+		num_test( shadow( nums ), "shadow( nums )" );
 	}
 	DOCTEST_TEST_CASE( "litteral text" ) {
-		DOCTEST_FAST_CHECK_EQ ( std::format( "{}", litt( "text" ) ), "text" );
+		static_assert( litt( "text" ).last( 2 ) == "xt" );
+		DOCTEST_FAST_CHECK_EQ ( std::format( "{:?s}", shadow( "1\t2\n3\"4" ) ), "\"1\\t2\\n3\\\"4\"" );
+		text_test( litt( "text" ), "litt( \"text\" )" );
 
-		static_assert( litt( "text" )					== "text" );
-		// static_assert( litt( "text" )					>  "test" );
-		static_assert( litt( "text" )[ 4 ]				== 0 );
-		static_assert( *( litt( "text" ).begin() + 1 ) 	== 'e' );
-		static_assert( *( litt( "text" ).end()   - 2 ) 	== 'x' );
-		static_assert( litt( "text" ).front() 			== 't' );
-		static_assert( litt( "text" ).back() 			== 't' );
-		static_assert( litt( "text" ).size()			== 4u );
-		static_assert( litt( "text" ).first( 2 )		== "te" );
-		static_assert( litt( "text" ).first( 9 )		== "text" );
-		static_assert( litt( "text" ).last( 2 )			== "xt" );
-		static_assert( litt( "text" ).last( 9 )			== "text" );
-		static_assert( litt( "text" ).part( 1, 2 )		== "ex" );
-		static_assert( litt( "text" ).part( 9, 2 )		== "" );
-		static_assert( litt( "text" ).part( 1, 9 )		== "ext" );
-		static_assert( litt( "text" ).starts_with( "tex" ) );
-		static_assert( litt( "text" ).ends_with( "ext" ) );
-#if defined( __clang__ )	// These are not constexpr in gcc...
-		static_assert( litt( "text" ).contains( 'x' ) );
-		static_assert( litt( "text" ).contains( "xt" ) );
-#endif
 		// Test template usage.
 		static_assert( litteral_test< litt( "text" ) >::v	== "text" );
 	}
