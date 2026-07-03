44ea6eb00dd353b870fe77b68cf99adcffcd2010
Snuggan <peder@axensten.se>
2026-07-03 12:38:09 +0200

    Regressed to std::format (not fmt::format).

--- include/pax/std/format.hpp
+++ include/pax/std/format.hpp
@@ -9,20 +9,20 @@
 
 
 // Using std::format works well with clang 18. It does not work with gcc 13.2 or 14, however.
-#if ( defined( __GNUC__ ) && !defined( __clang__ ) && true ) || true
-	//	https://fmt.dev/latest/index.html
-	//	https://github.com/fmtlib/fmt
-#	define FMT_HEADER_ONLY
-#	include "../external/fmt/format.h"
-#	include "../external/fmt/chrono.h"
-#	include "../external/fmt/ostream.h"
-#	include "../external/fmt/ranges.h"
-#	undef std20
-#	define std20 fmt
-
-	namespace std20 {
-		/// When the format string is not known at compile time, std::format may not be used as usual.
-		/// Instead the format argument signals this case by having a specific type.
-		inline constexpr auto runtime_format( const std::string_view fmt_ )	{	return runtime( fmt_ );		}
-	}
-#endif
+// #if ( defined( __GNUC__ ) && !defined( __clang__ ) && true ) || true
+// 	//	https://fmt.dev/latest/index.html
+// 	//	https://github.com/fmtlib/fmt
+// #	define FMT_HEADER_ONLY
+// #	include "../external/fmt/format.h"
+// #	include "../external/fmt/chrono.h"
+// #	include "../external/fmt/ostream.h"
+// #	include "../external/fmt/ranges.h"
+// #	undef std20
+// #	define std20 fmt
+//
+// 	namespace std20 {
+// 		/// When the format string is not known at compile time, std::format may not be used as usual.
+// 		/// Instead the format argument signals this case by having a specific type.
+// 		inline constexpr auto runtime_format( const std::string_view fmt_ )	{	return runtime( fmt_ );		}
+// 	}
+// #endif
