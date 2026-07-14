d0d7f7d85324249a213dfe41818087e1e17df910
Snuggan <peder@axensten.se>
2026-07-14 16:22:45 +0200

    Compiles, but untested.

--- include/pax/pdal/modules/pdal_plugin_filter_plot_points2.hpp
+++ include/pax/pdal/modules/pdal_plugin_filter_plot_points2.hpp
@@ -5,36 +5,59 @@
 #include <pdal/Filter.hpp>
 #include <pdal/Streamable.hpp>
 #include <string>
+#include <string_view>
 
 
 namespace pax {
 
+	class Plot_points;	// In pax/pdal/process/plot-points.hpp
+	class Plot_points;
+	
 
 	/// Process point cloud files into individual plot point cloud files. 
 	class PDAL_DLL plot_points : public pdal::Filter, public pdal::Streamable {
 	public:
-		plot_points()			  = default;
+		plot_points()									  = default;
+		plot_points( const plot_points & )				  = delete;
+		plot_points( plot_points &&)					  = delete;
+		plot_points & operator=( const plot_points & )	  = delete;
+		plot_points & operator=( plot_points && )		  = delete;
+
 		virtual ~plot_points();
 		std::string getName()								const override;
 
 	private:
-		void addArgs( pdal::ProgramArgs & )					override;
-		void addDimensions( pdal::PointLayoutPtr layout_ )	override;
-		bool processOne( pdal::PointRef & pt_ )				override;
-		pdal::PointViewSet run( pdal::PointViewPtr view_ )	override;
-
 		using coordinate_type	  = double;
 		using value_type		  = float;
 
-		struct metadata {
-			std::size_t 			points_processed{};
-		};
+		template< typename BBox >
+		static std::vector< Plot_points > get_plots(
+			const std::string_view	plots_table_, 
+			const std::string_view 	id_column_,
+			const coordinate_type 	max_disdance_,
+			const BBox			  & bbox_
+		);
 
-		std::string					m_plot_file{}, m_dest_plot_points_directory{}, 
-									m_id_column{ "id" }, m_dest_format{ ".laz" };
+		void addArgs( pdal::ProgramArgs & )					override;
+	    void prepared( pdal::PointTableRef table_ )			override;
+		bool processOne( pdal::PointRef & pt_ )				override;
+		pdal::PointViewSet run( pdal::PointViewPtr view_ )	override;
+
+		std::string					m_plot_file{}, 
+									m_dest_plot_points_directory{}, 
+									m_id_column{ "id" }, 
+									m_dest_format{ ".laz" };
 		double						m_buffer{ 0.0 };
 		pdal::SpatialReference		m_srs;
 		
+		pdal::PointViewPtr			m_view_ptr;
+		std::vector< Plot_points >	m_plots;		// Binary "table" of plots.
+		pdal::PointViewPtr			m_pt_view_ptr;
+		
+		struct metadata {
+			std::size_t 			plots_processed{}, points_processed{}, points_found{};
+		};
+
 		mutable metadata			m_metadata{};
 	};
 
--- pdal-plugins/pdal_plugin_filter_plot_points2.cpp
+++ pdal-plugins/pdal_plugin_filter_plot_points2.cpp
@@ -1,8 +1,12 @@
 #include <pax/pdal/modules/pdal_plugin_filter_plot_points2.hpp>
+#include <pax/tables/text-table.hpp>			// Handle a csv file.
+
 #include <pax/pdal/process/plot-points.hpp>
 
 #include <pdal/util/FileUtils.hpp>
 #include <pdal/util/ProgramArgs.hpp>
+
+#include <vector>
 
 
 namespace pax {
@@ -35,56 +39,94 @@ namespace pax {
 	}
 
 
-	void plot_points::addDimensions( pdal::PointLayoutPtr layout_ ) {
-		// Don't know if this is necessary, as we only use standard dimensions...
-		layout_->registerDim( pdal::Dimension::Id::X );
-		layout_->registerDim( pdal::Dimension::Id::Y );
-
-		// Check argumeents.
-		if( m_buffer < 0 )
-			std::cerr	<< error_message( "To export points in plots the radius needs to be non-negative." ).what() 
-						<< '\n';
-	}
-
-
 	plot_points::~plot_points() {
 		// Export metadata.
 		pdal::MetadataNode					meta = getMetadata();
+		meta.add( "plots-processed",		m_metadata.plots_processed );
 		meta.add( "points-processed",		m_metadata.points_processed );
+		meta.add( "points-found",			m_metadata.points_found );
 
 		pdal::MetadataNode					arguments( "arguments" );
 		arguments.add( "plot_file",			to_string( m_plot_file ) );
 		arguments.add( "dest_plot_points",	to_string( m_dest_plot_points_directory ) );
 		arguments.add( "max_distance",		m_buffer );
 		meta.add( arguments );
+		
+		try {
+			for( const auto & plot : m_plots )	if( !plot.empty() )				
+				plot.save( m_pt_view_ptr, m_dest_plot_points_directory, m_dest_format );
+		} catch( const std::exception & error_ ) {
+			std::cerr << error_.what() << '\n';
+			throw;	// Or should we just terminate?
+		}
 	}
 
 
-	bool plot_points::processOne( pdal::PointRef &  ) {
+	// Read a plot file and return a vector of the plots in the bbox.
+	template< typename BBox >
+	std::vector< Plot_points > plot_points::get_plots(
+		const std::string_view				plots_table_, 
+		const std::string_view 				id_column_,
+		const coordinate_type				buffer_,
+		const BBox						  & bbox_
+	) {
+		std::vector< Plot_points >			plots{};
+
+		// First, read in all plots from the csv file.
+		if( !empty( bbox_ ) ) {	
+			Text_table< char >				plots_table{ plots_table_ };
+			plots						  = plots_table.export_values( Plot_points::table_meta( id_column_ ) );
+		}
+		
+		// Then, only keep the plots that overlap the bbox_.
+		// Copy the relevant plots to the begining of 'plots' and...
+		auto itr						  = plots.begin();
+		for( Plot_points & plot : plots )
+			if( plot.in_box( bbox_ ) )	  * ( itr++ ) = plot;
+
+		// ...resize it to contain just those relevant plots.
+		plots.resize( std::size_t( itr - plots.begin() ) );
+		plots.shrink_to_fit();
+
+		// If so stated, set the buffer to the plots.
+		if( buffer_ > 0 ) for( Plot_points & plot : plots )
+			plot.set_radius( buffer_ );
+
+		return plots;
+	}
+
+
+	/// Do pre-flight stuff.
+	void plot_points::prepared( pdal::PointTableRef pt_table_ ) {
+		m_plots							  = get_plots( m_plot_file, m_id_column, m_buffer, bbox( pt_table_ ) );
+		m_metadata.plots_processed		  = m_plots.size();
+		m_pt_view_ptr					  = std::make_shared< pdal::PointView >( pt_table_ );
+		
+
+		// Check argumeents.
+		if( m_buffer < 0 )		std::cerr
+			<< error_message( "To export points in plots the radius needs to be non-negative." ).what() 
+			<< '\n';
+	}
+
+
+	bool plot_points::processOne( pdal::PointRef & pt_ ) {
 		++m_metadata.points_processed;
+		for( auto & plot : m_plots )		m_metadata.points_found += plot.process( pt_ );
 		return true;
 	}
 
 
 	pdal::PointViewSet plot_points::run( pdal::PointViewPtr view_ ) {
-		pdal::PointViewSet					result;
-		try {
-			Process_plots_points::process( 
-				view_, 
-				m_plot_file, 
-				m_dest_plot_points_directory, 
-				m_buffer, 
-				m_id_column, 
-				m_dest_format
-			);
-			m_metadata.points_processed = view_->size();
-
-			result.insert( view_ );
-
-		} catch( const std::exception & error_ ) {
-			std::cerr << error_.what() << '\n';
-			throw;
+		// Process the points.
+		auto pt = view_->point( 0 );
+		for( pdal::PointId idx = 0; idx < view_->size(); ++idx ) {
+			pt = view_->point( idx );
+			processOne( pt );
 		}
+
+		pdal::PointViewSet				result;
+		result.insert( view_ );
 		return result;
 	}
 
