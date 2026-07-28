92720a27555e68a9a0e96162de2cc6fb0d62be27
Snuggan <peder@axensten.se>
2026-07-28 13:45:46 +0200

    Estetics.

--- include/pax/types/point-stuff/distance.hpp
+++ include/pax/types/point-stuff/distance.hpp
@@ -24,9 +24,9 @@ namespace pax {
 	template< std::size_t P, sized_contiguous Cont > 
 		requires( all_arithmetic< Cont > && all_static_extent< Cont > )
 	constexpr auto minkowskiP( 
-		Cont		 && pt_
+		Cont		 && v_
 	) noexcept {
-		auto [ ... p ]		  = pt_;
+		auto [ ... p ]		  = v_;
 		if constexpr( P == 0 )	return std::max({ 0, ( ( p >= 0 ) ? p : -p ) ... });
 		else					return ( abs_power( p, P ) + ... + 0 );
 	}
@@ -38,12 +38,12 @@ namespace pax {
 	template< std::size_t P, sized_contiguous Cont0, sized_contiguous Cont1 >
 		requires( all_arithmetic< Cont0, Cont1 > && all_same_static_extent< Cont0, Cont1 > )
 	constexpr auto minkowskiP(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) noexcept {
 		static constexpr const auto absdiff = []( auto a, auto b ){	return ( a > b ) ? a-b : b-a;	};
-		auto [ ... p0 ] = pt0_;	
-		auto [ ... p1 ] = pt1_;
+		auto [ ... p0 ] = v0_;	
+		auto [ ... p1 ] = v1_;
 		if constexpr( P == 0 )	return std::max({ 0, absdiff( p0, p1 ) ... });
 		else					return ( power( absdiff( p0, p1 ), P ) + ... + 0 );
 	}
@@ -55,11 +55,11 @@ namespace pax {
 	template< std::size_t P, sized_contiguous ... Cont >
 		requires( all_arithmetic< Cont ... > && all_static_extent< Cont ... > )
 	constexpr auto minkowski(
-		Cont		 && ... pt_
+		Cont		 && ... v_
 	) noexcept {
 		using F = std::common_type_t< double, traits::value_type_t< Cont > ... >;
-		if constexpr ( P == 0 )	return			minkowskiP< P >( std::forward< Cont >( pt_ )... );
-		else					return root( F( minkowskiP< P >( std::forward< Cont >( pt_ )... ) ), P );
+		if constexpr ( P == 0 )	return			minkowskiP< P >( std::forward< Cont >( v_ )... );
+		else					return root( F( minkowskiP< P >( std::forward< Cont >( v_ )... ) ), P );
 	}
 
 	/// Chebyshev distance (minkowski distance, with infinite P).
@@ -68,7 +68,7 @@ namespace pax {
 	/// - (The containers must have the same number of elements, as the distance is defined that way.) 
 	/// - Must have a static extent, the present implementation depends on it. 
 	template< sized_contiguous ... Cont >		requires( all_arithmetic< Cont ... > && all_static_extent< Cont ... > )
-	constexpr auto chebyshev ( Cont && ...t_ )	noexcept	{	return minkowski < 0 >( std::forward< Cont >( t_ )... );	}
+	constexpr auto chebyshev ( Cont && ... v_ )	noexcept	{	return minkowski < 0 >( std::forward< Cont >( v_ ) ... );	}
 
 	/// Manhattan distance (minkowski distance, with P = 1).
 	/// The sum of the absolute differences between the coordinates of two points.
@@ -76,7 +76,7 @@ namespace pax {
 	/// - (The containers must have the same number of elements, as the distance is defined that way.) 
 	/// - Must have a static extent, the present implementation depends on it. 
 	template< sized_contiguous ... Cont >		requires( all_arithmetic< Cont ... > && all_static_extent< Cont ... > )
-	constexpr auto manhattan ( Cont && ...t_ )	noexcept	{	return minkowski < 1 >( std::forward< Cont >( t_ )... );	}
+	constexpr auto manhattan ( Cont && ... v_ )	noexcept	{	return minkowski < 1 >( std::forward< Cont >( v_ ) ... );	}
 
 	/// Euclidean distance (minkowski distance, with P = 2).
 	/// The Euclidean distance is the straight-line distance between two points in a flat space.
@@ -84,7 +84,7 @@ namespace pax {
 	/// - (The containers must have the same number of elements, as the distance is defined that way.) 
 	/// - Must have a static extent, the present implementation depends on it. 
 	template< sized_contiguous ... Cont >		requires( all_arithmetic< Cont ... > && all_static_extent< Cont ... > )
-	constexpr auto euclidean ( Cont && ...t_ )	noexcept	{	return minkowski < 2 >( std::forward< Cont >( t_ )... );	}
+	constexpr auto euclidean ( Cont && ... v_ )	noexcept	{	return minkowski < 2 >( std::forward< Cont >( v_ ) ... );	}
 
 	/// Squared euclidean distance (squared minkowski distance, with P = 2).
 	/// Same as the euclidean distance squared. Useful when comparing distances. 
@@ -92,7 +92,7 @@ namespace pax {
 	/// - (The containers must have the same number of elements, as the distance is defined that way.) 
 	/// - Must have a static extent, the present implementation depends on it. 
 	template< sized_contiguous ... Cont >		requires( all_arithmetic< Cont ... > && all_static_extent< Cont ... > )
-	constexpr auto euclidean2( Cont && ...t_ )	noexcept	{	return minkowskiP< 2 >( std::forward< Cont >( t_ )... );	}
+	constexpr auto euclidean2( Cont && ... v_ )	noexcept	{	return minkowskiP< 2 >( std::forward< Cont >( v_ ) ... );	}
 
 
 	/// Canberra distance.
@@ -103,15 +103,15 @@ namespace pax {
 	template< sized_contiguous Cont0, sized_contiguous Cont1 >
 	requires( all_arithmetic< Cont0, Cont1 > && all_same_static_extent< Cont0, Cont1 > )
 	constexpr auto canberra(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) noexcept {
 		using F = std::common_type_t< double, traits::value_type_t< Cont0 >, traits::value_type_t< Cont1 > >;
 		constexpr const auto f = []( F a, F b ) {	// absdiff( a, b ) / (	abs( a ) + abs( b ) ): 
 			return ( ( a > b ) ? a-b : b-a ) / ( ( ( a >= F{} ) ? a : -a ) + ( ( b >= F{} ) ? b : -b ) );
 		};
-		auto [ ...t0 ] = pt0_;
-		auto [ ...t1 ] = pt1_;
+		auto [ ...t0 ] = v0_;
+		auto [ ...t1 ] = v1_;
 		return ( F{} + ... + f( t0, t1 ) );
 	}
 
@@ -123,15 +123,15 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( all_arithmetic< Cont0, Cont1 > )
 	constexpr double cosine(
-		Cont0		 && a_,
-		Cont1		 && b_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) {
 		using 		std::begin, std::size;
 		double		dot{}, nmA{}, nmB{};
-		auto		itrA = begin( a_ );
-		auto		itrB = begin( b_ );
-		const auto	endA = no_nullchar_end( a_ );
-		assert( ( endA - itrA == no_nullchar_end( b_ ) - itrB ) && "The containers must have the same number of elements." );
+		auto		itrA = begin( v0_ );
+		auto		itrB = begin( v1_ );
+		const auto	endA = no_nullchar_end( v0_ );
+		assert( ( endA - itrA == no_nullchar_end( v1_ ) - itrB ) && "The containers must have the same number of elements." );
 		while( itrA < endA ) {
 			dot	 += *itrA * *itrB;
 			nmA	 += *itrA * *itrA;
@@ -149,19 +149,19 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > )
 	constexpr std::size_t kendall_tau(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) noexcept {
 		using std::begin, std::size;
 		std::size_t 		count{};
-		const std::size_t 	n = no_nullchar_size( pt0_ );
+		const std::size_t 	n = no_nullchar_size( v0_ );
 
-		assert( ( n == no_nullchar_size( pt1_) ) && "The containers must have the same number of elements." );
+		assert( ( n == no_nullchar_size( v1_) ) && "The containers must have the same number of elements." );
 		for( std::size_t i = 0; i < n; ++i )
 			for( std::size_t j = i + 1; j < n; ++j )
 				// Check if the order is different in the two rankings
-				count	+=	( pt0_[ i ] < pt0_[ j ] && pt1_[ i ] > pt1_[ j ] ) 
-						||	( pt0_[ i ] > pt0_[ j ] && pt1_[ i ] < pt1_[ j ] );
+				count	+=	( v0_[ i ] < v0_[ j ] && v1_[ i ] > v1_[ j ] ) 
+						||	( v0_[ i ] > v0_[ j ] && v1_[ i ] < v1_[ j ] );
 		return count;
 	}
 
@@ -172,11 +172,11 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > )
 	constexpr double normalized_kendall_tau(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) noexcept {
 		using std::size;
-		return kendall_tau( pt0_, pt1_ )*2.0 / ( size( pt0_ )*( size( pt0_ ) - 1.0 ) );
+		return kendall_tau( v0_, v1_ )*2.0 / ( size( v0_ )*( size( v0_ ) - 1.0 ) );
 	}
 
 
@@ -188,11 +188,11 @@ namespace pax {
 	template< sized_contiguous Cont0, sized_contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > && all_same_static_extent< Cont0, Cont1 > )
 	constexpr std::size_t hamming(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) noexcept {
-		auto [ ...t0 ] = pt0_;
-		auto [ ...t1 ] = pt1_;
+		auto [ ...t0 ] = v0_;
+		auto [ ...t1 ] = v1_;
 		return ( int{} + ... + ( t0 != t1 ) );
 	}
 	
@@ -203,16 +203,16 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > )
 	constexpr double sorensen_dice(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) noexcept {
 		using std::begin, std::size;
 		std::size_t 		matches{};
-		auto				itr0 = begin( pt0_ );
-		auto				itr1 = begin( pt1_ );
-		const auto			end0 = no_nullchar_end( pt0_ ) - 1;
+		auto				itr0 = begin( v0_ );
+		auto				itr1 = begin( v1_ );
+		const auto			end0 = no_nullchar_end( v0_ ) - 1;
 		const std::size_t 	len  = end0 - itr0;
-		assert( ( end0 - itr0 + 1 == no_nullchar_end( pt1_ ) - itr1 ) && "The containers must have the same number of elements." );
+		assert( ( end0 - itr0 + 1 == no_nullchar_end( v1_ ) - itr1 ) && "The containers must have the same number of elements." );
 		if( itr0 + 1u > end0 )	return 0.0;
 
 		while( itr0 < end0 ) {
@@ -229,16 +229,16 @@ namespace pax {
 	template< contiguous Cont >
 		requires( uinteger< traits::value_type_t< Cont > > )
 	double shannon_index(
-		Cont		 && pt_
+		Cont		 && v_
 	) {
 		double			H{};
 		std::size_t		total{};
 
 		// Total number of individuals
-		for( std::size_t count : pt_ )		total += count;
+		for( std::size_t count : v_ )		total += count;
 
 		// Calculate Shannon's index
-		for( std::size_t count : pt_ ) 
+		for( std::size_t count : v_ ) 
 			if( count > 0 ) {
 				const double p = static_cast< double >( count )/total;
 				H			  -= p * std::log( p );
@@ -247,12 +247,12 @@ namespace pax {
 	}
 	
 
-	/// First sorts a copy of cont_ and then remove doubles. Returns a std::vector.
+	/// First sorts a copy of v_ and then remove doubles. Returns a std::vector.
 	template< contiguous Cont >
-	constexpr auto sorted_unique( Cont && cont_ ) {
+	constexpr auto sorted_unique( Cont && v_ ) {
 		using std::begin, std::size;
-		std::vector< traits::value_type_t< Cont > >		arr( size( cont_ ) );
-		std::copy( begin( cont_ ), no_nullchar_end( cont_ ), arr.begin() );
+		std::vector< traits::value_type_t< Cont > >		arr( no_nullchar_size( v_ ) );
+		std::copy( begin( v_ ), no_nullchar_end( v_ ), arr.begin() );
 		std::sort( arr.begin(), arr.end() );
 		const auto itr = std::unique( arr.begin(), arr.end() );
 		arr.resize( itr - arr.begin() );
@@ -265,14 +265,14 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > )
 	constexpr double jaccard(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) noexcept {
 		std::size_t 	common{};
-		const auto		arr0 = sorted_unique( pt0_ );
+		const auto		arr0 = sorted_unique( v0_ );
 		auto			itr0 = arr0.begin();
 		const auto		end0 = no_nullchar_end( arr0 );
-		const auto		arr1 = sorted_unique( pt1_ );
+		const auto		arr1 = sorted_unique( v1_ );
 		auto			itr1 = arr1.begin();
 		const auto		end1 = no_nullchar_end( arr1 );
 		while( ( itr0 < end0 ) && ( itr1 < end1 ) ) {
@@ -293,11 +293,11 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > )
 	constexpr double jaro_similarity(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) {
-		const std::size_t 		sz0 = no_nullchar_size( pt0_ );
-		const std::size_t 		sz1 = no_nullchar_size( pt1_ );
+		const std::size_t 		sz0 = no_nullchar_size( v0_ );
+		const std::size_t 		sz1 = no_nullchar_size( v1_ );
 	
 		if( !sz0 && !sz1 )		return 1.0;		// Both strings are empty
 		if( !sz0 || !sz1 )		return 0.0;		// One string is empty
@@ -313,7 +313,7 @@ namespace pax {
 				 			 j < std::min( sz1, i + max_dist + 1 ); 
 							 ++j 
 			) {
-				if( !matched2[ j ] && ( pt0_[ i ] == pt1_[ j ] ) ) {
+				if( !matched2[ j ] && ( v0_[ i ] == v1_[ j ] ) ) {
 					matched1[ i ] = matched2[ j ] = true;
 					++matches;
 					break;
@@ -328,7 +328,7 @@ namespace pax {
 		for( std::size_t i = 0; i < sz0; ++i ) {
 			if( matched1[ i ] ) {
 				while( !matched2[ pt ] )	++pt;
-				t 			 += pt0_[ i ] != pt1_[ pt ];
+				t 			 += v0_[ i ] != v1_[ pt ];
 				++pt;
 			}
 		}
@@ -348,16 +348,16 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > )
 	constexpr double jaro_winkler_similarity(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) {
 		// Calculate common prefix length (up to 4 characters)
 		std::size_t 			prefix_length{};
-		const std::size_t 		stop = std::min({ std::size_t( 4 ), no_nullchar_size( pt0_ ), no_nullchar_size( pt1_ ) });
-		for( std::size_t i = 0; i < stop && ( pt0_[ i ] == pt1_[ i ] ); ++i ) 
+		const std::size_t 		stop = std::min({ std::size_t( 4 ), no_nullchar_size( v0_ ), no_nullchar_size( v1_ ) });
+		for( std::size_t i = 0; i < stop && ( v0_[ i ] == v1_[ i ] ); ++i ) 
 			++prefix_length;
 
-		const double			jaro_sim = jaro_similarity( pt0_, pt1_ );
+		const double			jaro_sim = jaro_similarity( v0_, v1_ );
 		return jaro_sim + ( 0.1 * prefix_length * ( 1.0 - jaro_sim ) );
 	}
 
@@ -369,15 +369,15 @@ namespace pax {
 	template< contiguous Cont0, contiguous Cont1 >
 		requires( pairwise_comparable< Cont0, Cont1 > )
 	constexpr std::size_t levenshtein(
-		Cont0		 && pt0_,
-		Cont1		 && pt1_
+		Cont0		 && v0_,
+		Cont1		 && v1_
 	) {
 		using std::data, std::size_t;
-		if( size( pt1_ ) < size( pt0_ ) )	levenshtein( pt1_, pt0_ );
+		if( size( v1_ ) < size( v0_ ) )	levenshtein( v1_, v0_ );
 
 		using Idx			  = std::size_t;
-		const Idx cols		  = size( pt0_ ) + 1u;
-		const Idx rows		  = size( pt1_ ) + 1u;
+		const Idx cols		  = size( v0_ ) + 1u;
+		const Idx rows		  = size( v1_ ) + 1u;
 
 		using Int = std::common_type_t< traits::value_type_t< Cont0 >, traits::value_type_t< Cont1 > >;
 		std::vector< Int >		t( 2*cols );
@@ -390,7 +390,7 @@ namespace pax {
 			*now			  = r;
 			for( Idx c{ 1 }; c<cols; ++c ) {
 				now[ c ]	  = std::min({
-					prev[ c - 1u ] + ( pt0_[ c - 1u ] != pt1_[ r - 1u ] ),
+					prev[ c - 1u ] + ( v0_[ c - 1u ] != v1_[ r - 1u ] ),
 					prev[ c	  ] + 1,
 					now [ c - 1u ] + 1
 				});
